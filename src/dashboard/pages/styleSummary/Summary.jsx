959008342340bc5a94f6c821466fbaaa06ef1b94
Riad Sarkar <riadsarkar45@gmail.com>
2026-05-15 15:04:40 +0600

    modal removed from summary.jsx

--- src/dashboard/pages/styleSummary/Summary.jsx
+++ src/dashboard/pages/styleSummary/Summary.jsx
@@ -62,11 +62,6 @@ export default function Summary() {
             {showModal && <StyleReqModal setRawData={setRawData} setShowModal={setShowModal} />}
 
 
-            <div>
-                <Deliveries />
-            </div>
-
-
             <div className="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default">
                 <table className="w-full text-sm text-left rtl:text-right text-body">
                     <thead className="text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default">
