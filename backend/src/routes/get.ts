0d21f20e9e2a41b2c4d5304e2895d5e21681b296
riadsarkar45 <riadsarkar45@gmail.com>
2026-07-05 15:18:22 +0600

    backend get api's secure with authenticate and authorization

--- backend/src/routes/get.ts
+++ backend/src/routes/get.ts
@@ -10,30 +10,30 @@ import { apiLimiter } from "../middleware/rateLimiter/apiLimiter";
 import { getDeliveryData } from "../controllers/deliveries/getDeliveryData";
 import { deleteChallanFromDelivery } from "../controllers/deliveries/deleteDelivery";
 import { GlanceReport } from "../controllers/Glance/atGlanceReport";
-import { authenticate } from "../middleware/Authenticate.middleware";
+import { authenticate, authorize } from "../middleware/Authenticate.middleware";
 
 const getRouters = express.Router();
 
 console.log("getRouters loaded");
 
-getRouters.get("/work-order/:orderType", apiLimiter, authenticate, getAllOrders);
+getRouters.get("/work-order/:orderType", apiLimiter, authenticate, authorize("SUPER ADMIN", "ADMIN", "AUDITOR", "FACTORY AUDITOR"), getAllOrders);
 
-getRouters.get("/dashboard-detail", apiLimiter, dashboardController);
+getRouters.get("/dashboard-detail", apiLimiter, authenticate, authorize("SUPER ADMIN", "ADMIN", "AUDITOR", "FACTORY AUDITOR"), dashboardController);
 
-getRouters.get("/style-requirement", apiLimiter, getOrderSummaryByStyle);
+getRouters.get("/style-requirement", apiLimiter, authenticate, authorize("SUPER ADMIN", "ADMIN", "AUDITOR", "FACTORY AUDITOR"), getOrderSummaryByStyle);
 
-getRouters.get("/audits", apiLimiter, allAudits);
+getRouters.get("/audits", apiLimiter, authenticate, authorize("SUPER ADMIN", "ADMIN", "AUDITOR", "FACTORY AUDITOR"), allAudits);
 
-getRouters.get("/jobs", apiLimiter, getAllJobs);
+getRouters.get("/jobs", apiLimiter, authenticate, authorize("SUPER ADMIN", "ADMIN", "AUDITOR", "FACTORY AUDITOR"), getAllJobs);
 
-getRouters.get("/styles", apiLimiter, styleRequirements);
+getRouters.get("/styles", apiLimiter, authenticate, authorize("SUPER ADMIN", "ADMIN", "AUDITOR", "FACTORY AUDITOR"), styleRequirements);
 
-getRouters.get("/styles/:jobNo", apiLimiter, styleRequirements);
+getRouters.get("/styles/:jobNo", apiLimiter, authenticate, authorize("SUPER ADMIN", "ADMIN", "AUDITOR", "FACTORY AUDITOR"), styleRequirements);
 
-getRouters.get("/deliveries/:orderType", apiLimiter, deliveryDetail);
+getRouters.get("/deliveries/:orderType", apiLimiter, authenticate, authorize("SUPER ADMIN", "ADMIN", "AUDITOR", "FACTORY AUDITOR"), deliveryDetail);
 
-getRouters.delete("/delete-delivery/:deliveryId", deleteChallanFromDelivery)
+getRouters.delete("/delete-delivery/:deliveryId", authenticate, authorize("SUPER ADMIN", "ADMIN", "AUDITOR", "FACTORY AUDITOR"), deleteChallanFromDelivery)
 
-getRouters.get("/glance-report", GlanceReport);
+getRouters.get("/glance-report",  authenticate, authorize("SUPER ADMIN", "ADMIN", "AUDITOR", "FACTORY AUDITOR"),GlanceReport);
 
 export default getRouters;
