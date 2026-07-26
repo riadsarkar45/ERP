da9fdcc98563b711805af51d9b95b31811a2d28c
riadsarkar45 <riadsarkar45@gmail.com>
2026-07-26 12:59:27 +0600

    prisma relation rmeoved from style requirement to jobs model due to style requirement doesnt' have any unique data

--- backend/fix.sql
+++ backend/fix.sql
@@ -1,5 +1,4 @@
-ALTER TABLE "Composition" ALTER COLUMN "workOrderId" DROP NOT NULL;
-
-ALTER TABLE "Composition" DROP CONSTRAINT "Composition_workOrderId_fkey";
-ALTER TABLE "Composition" ADD CONSTRAINT "Composition_workOrderId_fkey"
-  FOREIGN KEY ("workOrderId") REFERENCES "WorkOrder"("id") ON DELETE SET NULL;
+SELECT w.id, w."workOrderNo", w."jobNo"
+FROM "WorkOrder" w
+LEFT JOIN "StyleRequirement" sr ON sr."jobNo" = w."jobNo"
+WHERE sr."jobNo" IS NULL;
