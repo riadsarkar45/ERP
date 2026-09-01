8a8f579dc36933250cd794f686757ebdf5f8e69e
Md. Dulu Sarker <dulu.sarker1992.@gmail.com>
2026-09-01 19:21:31 +0600

    @ testing)

--- src/hooks/Axios.jsx
+++ src/hooks/Axios.jsx
@@ -6,6 +6,7 @@ const axiosPublic = axios.create({
     //  baseURL: "https://erp-7-44h5.onrender.com",
     // baseURL: "http://localhost:3000",
     withCredentials: true,
+    
 });
 
 const useAxiosPublic = () => {
